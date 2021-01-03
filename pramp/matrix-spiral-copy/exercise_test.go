ec9222985e1531b745dc3ef11c8e75576216d40d
Andrey Karpov <ndk@users.noreply.github.com>
2021-01-03 12:25:17 +0300

    pramp: Matrix Spiral Copy

---
+++ pramp/matrix-spiral-copy/exercise_test.go
@@ -0,0 +1,78 @@
+package exercise
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+var (
+	cases = []struct {
+		matrix [][]int
+		output []int
+	}{
+		{
+			[][]int{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9},
+			},
+			[]int{1, 2, 3, 6, 9, 8, 7, 4, 5},
+		},
+		{
+			[][]int{
+				{1, 2, 3, 4},
+				{5, 6, 7, 8},
+				{9, 10, 11, 12},
+			},
+			[]int{1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7},
+		},
+		{
+			[][]int{
+				{1, 2, 3, 4, 5},
+				{6, 7, 8, 9, 10},
+				{11, 12, 13, 14, 15},
+				{16, 17, 18, 19, 20},
+			},
+			[]int{1, 2, 3, 4, 5, 10, 15, 20, 19, 18, 17, 16, 11, 6, 7, 8, 9, 14, 13, 12},
+		},
+	}
+
+	implementations = []struct {
+		name string
+		f    func(matrix [][]int) []int
+	}{
+		{"spiralCopy", spiralCopy},
+	}
+)
+
+func TestExercise(t *testing.T) {
+	Convey("Test", t, func() {
+		for i, c := range cases {
+			ca := c
+			Convey(fmt.Sprintf("case %d", i), func() {
+				for _, impl := range implementations {
+					f := impl.f
+					Convey(impl.name, func() {
+						result := f(ca.matrix)
+						So(result, ShouldResemble, ca.output)
+					})
+				}
+			})
+		}
+	})
+}
+
+func BenchmarkExercise(b *testing.B) {
+	for _, implementation := range implementations {
+		f := implementation.f
+		b.Run(implementation.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				for _, c := range cases {
+					f(c.matrix)
+				}
+			}
+		})
+	}
+}
